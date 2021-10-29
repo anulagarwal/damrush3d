b6d4f5b0688317ffd4f12475c6cda02925d3d41c
Anul Agarwal <anulagarwal12@gmail.com>
2021-10-29 14:18:46 +0530

    Changes

---
+++ Assets/Scripts/Support/GameManager.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Properties
+    public static GameManager Instance = null;
+
+    [Header("Component Reference")]
+    [SerializeField] public List<GameObject> controlObjects;
+    [SerializeField] public GameObject confetti;
+    [SerializeField] public GameObject cross;
+    [SerializeField] GameObject cineCam;
+    [SerializeField] GameObject mainCam;
+
+
+
+    [Header("Attributes")]
+    [SerializeField] private int currentScore;
+    [SerializeField] private int currentLevel;
+    [SerializeField] private int maxLevels;
+    [SerializeField] GameState currentState;
+
+
+    #endregion
+
+    #region MonoBehaviour Functions
+    private void Awake()
+    {
+        Application.targetFrameRate = 100;
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        //SwitchCamera(CameraType.MatchStickCamera);
+        currentLevel = PlayerPrefs.GetInt("level", 1);
+        foreach (GameObject g in controlObjects)
+        {
+            g.SetActive(false);
+        }
+        UIManager.Instance.UpdateLevel(currentLevel);
+        currentState = GameState.Main;
+        maxLevels = 3;
+    }
+    #endregion
+
+    public void StartLevel()
+    {
+        UIManager.Instance.SwitchUIPanel(UIPanelState.Gameplay);
+        foreach (GameObject g in controlObjects)
+        {
+            g.SetActive(true);
+        }
+        currentState = GameState.InGame;
+
+    }
+    public void SwitchToMainCam()
+    {
+        cineCam.SetActive(false);
+        mainCam.SetActive(true);
+
+    }
+
+    public void WinLevel()
+    {
+        if (currentState == GameState.InGame)
+        {
+            confetti.SetActive(true);
+            Invoke("ShowWinUI", 1.4f);
+            foreach (GameObject g in controlObjects)
+            {
+                g.SetActive(false);
+            }
+            
+            currentState = GameState.Win;
+
+            PlayerPrefs.SetInt("level", currentLevel + 1);
+            currentLevel++;
+        }
+    }
+
+    public void LoseLevel()
+    {
+        if (currentState == GameState.InGame)
+        {
+            //Show cat VFX
+            foreach (GameObject g in controlObjects)
+            {
+                g.SetActive(false);
+            }
+
+            Invoke("ShowLoseUI", 2f);
+            currentState = GameState.Lose;
+        }
+    }
+
+    #region Scene Management
+
+
+
+    public void ChangeLevel()
+    {
+        if (currentLevel > maxLevels)
+        {
+            int newId = currentLevel % maxLevels;
+            if (newId == 0)
+            {
+                newId = maxLevels;
+            }
+            SceneManager.LoadScene("Level " + (newId));
+        }
+        else
+        {
+            SceneManager.LoadScene("Level " + currentLevel);
+        }
+    }
+
+    #endregion
+
+
+    #region Public Core Functions
+
+    public void AddScore(int value)
+    {
+        currentScore += value;
+        UIManager.Instance.UpdateScore(currentScore);
+    }
+
+    public void SpawnCross(Vector3 pos, Transform t)
+    {
+        GameObject g = Instantiate(cross, pos, Quaternion.identity);
+        g.transform.parent = t;
+    }
+
+
+
+    #endregion
+
+    #region Invoke Functions
+
+    void ShowWinUI()
+    {
+        UIManager.Instance.SwitchUIPanel(UIPanelState.GameWin);
+    }
+
+    void ShowLoseUI()
+    {
+        UIManager.Instance.SwitchUIPanel(UIPanelState.GameLose);
+    }
+    #endregion
+}
